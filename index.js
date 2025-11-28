c2e4e0186100a7dbe5a81329d0dea8f7284542fc
Vidar Eldøy <vidar@eldoy.com>
2025-11-28 20:51:58 +0100

    new: js types demo

---
+++ index.js
@@ -0,0 +1,16 @@
+var { handleUser } = require('./lib/user.js')
+
+/** @type {User} */
+var user = {}
+
+// or inline like this:
+// var user /** @type {User} */ = {}
+
+// Should show error
+user.hello = 'hello'
+
+// Autocomplete works
+// user.
+
+// Error if used directly
+handleUser({ id: '1', name: 'tekki' })
---
+++ types/user.d.ts
@@ -0,0 +1,10 @@
+type User = {
+  id: string
+  email: string
+  age: number
+  createdAt: Date
+  profile: {
+    bio: string
+    createdAt: Date
+  }
+}
